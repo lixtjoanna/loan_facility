0b55fc275f9e6ede43fc937754444facdd0f2aa5
xiutingli <lixtjoanna@gmail.com>
2019-02-16 23:01:55 -0800

    fixing test cases

--- src/simulation.py
+++ src/simulation.py
@@ -4,7 +4,7 @@ import logging
 
 DEV_PLAN = True if 'DEV_PLAN' in os.environ else False
 file_source = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'small' if DEV_PLAN else 'large')
-YIELD_FILE_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'output/yield.csv')
+YIELD_FILE_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'output/yields.csv')
 ASSIGNMENT_FILE_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'output/assignments.csv')
 
 
@@ -38,10 +38,10 @@ class FacilityAssignment(object):
             'max_default_likelihood': 'sum',
             'banned_state': lambda x: tuple(x)
         })
-        logging.debug('Convert covenants data frame to shape {}'.format(covenants_df.shape))
+        logging.info('Convert covenants data frame to shape {}'.format(covenants_df.shape))
         # inner join facility table and covenant table with facility id
         self.bank_and_facility_df = facilities_df.join(covenants_df, on=['id'])
-        logging.debug('Merged covenants to facility table with shape {}'.format(self.bank_and_facility_df.shape))
+        logging.info('Merged covenants to facility table with shape {}'.format(self.bank_and_facility_df.shape))
 
         # join the data frame with bank name
         # also rename some columns for better human understanding
@@ -70,7 +70,7 @@ class FacilityAssignment(object):
             expected_yield = int((1 - default_likelihood) * loan_interest_rate * amount - \
                              default_likelihood * amount - \
                              facility_interest_rate * amount)
-            logging.debug('Facililty %s will add new expected_yield %s' % (facility_id, expected_yield))
+            logging.info('Facililty %s will add new expected_yield %s' % (facility_id, expected_yield))
             self.__write_to_file(int(facility_id), str(id), int(expected_yield))
             self.__reduce_amount_from_facility(facility_id, int(amount))
         else:
@@ -83,10 +83,10 @@ class FacilityAssignment(object):
         yield_df = pd.read_csv(YIELD_FILE_PATH)
 
         if not yield_df[yield_df.facility_id == facility_id].empty:
-            logging.debug('Facility number %s found in record, will update the expected yield value' % facility_id)
+            logging.info('Facility number %s found in record, will update the expected yield value' % facility_id)
             yield_df.loc[yield_df.facility_id == facility_id, 'expected_yield'] += expected_yield
         else:
-            logging.debug('Facility number %s not found in record, will create row' % facility_id)
+            logging.info('Facility number %s not found in record, will create row' % facility_id)
             yield_df = yield_df.append(pd.DataFrame([[facility_id, expected_yield]],
                                                     columns=['facility_id', 'expected_yield']))
 
@@ -95,6 +95,6 @@ class FacilityAssignment(object):
     def __reduce_amount_from_facility(self, facility_id,  value):
 
         old_val = self.bank_and_facility_df.loc[self.bank_and_facility_df.facility_id == facility_id]['amount']
-        logging.debug('Facility %s amount before is %s' % (facility_id, old_val))
+        logging.info('Facility %s amount before is %s' % (facility_id, old_val))
         self.bank_and_facility_df.loc[self.bank_and_facility_df.facility_id == facility_id, 'amount'] = old_val - value
-        logging.debug('Facility %s amount is updated to %s' %(facility_id, old_val - value))
+        logging.info('Facility %s amount is updated to %s' %(facility_id, old_val - value))
--- test/test_simulation.py
+++ test/test_simulation.py
@@ -2,22 +2,35 @@ import os
 import pytest
 from src.simulation import FacilityAssignment
 
-YIELD_FILE_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'output/yield.csv')
+YIELD_FILE_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'output/yields.csv')
 ASSIGNMENT_FILE_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'output/assignments.csv')
 
+TARGET_YIELD_FILE_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'small/yields.csv')
+TARGET_ASSIGNMENT_FILE_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'small/assignments.csv')
 
 @pytest.fixture
 def loan_csv():
     with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'small', 'loans.csv'), 'r') as loan_data:
         lines = loan_data.read().split('\n')
-    return lines
+    return lines[:-1]
 
 
 def test_simualtion(loan_csv):
+    if 'DEV_PLAN' not in os.environ:
+        assert False, 'Please run pytest with export DEV_PLAN'
     assignment = FacilityAssignment()
-    for line in loan_csv[1:-1]:
+    for line in loan_csv[1:]:
         interest_rate, amount, id, default_likelihood, state = line.split(',')
         assignment.simulation(float(interest_rate), int(amount), id, float(default_likelihood), state)
-    assert False
 
+    with open(YIELD_FILE_PATH, 'r') as data:
+        lines = data.read()
+        with open(TARGET_YIELD_FILE_PATH,'r') as target:
+            target_lines = target.read()
+            assert lines[:-1] == target_lines
 
+    with open(ASSIGNMENT_FILE_PATH, 'r') as data:
+        lines = data.read()
+        with open(TARGET_ASSIGNMENT_FILE_PATH,'r') as target:
+            target_lines = target.read()
+            assert lines == target_lines
